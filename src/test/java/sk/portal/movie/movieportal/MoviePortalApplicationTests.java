b930ce944b5020d118d4c2524da9598f626ad3f2
Jakub Remenec <jremenec@davincisoftware.sk>
2019-02-24 13:22:00 +0100

    #1 Initial project structure SpringBoot + Apache Maven

---
+++ src/test/java/sk/portal/movie/movieportal/MoviePortalApplicationTests.java
@@ -0,0 +1,16 @@
+package sk.portal.movie.movieportal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MoviePortalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
